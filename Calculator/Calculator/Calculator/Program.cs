2816a1f943fc0e91b4531999cc29276873bef831
Kacper <silynazamiary@gmail.com>
2024-12-11 13:18:12 +0100

    🌐3(Calculator(Tests, log4net))

---
+++ Calculator/Calculator/Calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
+
+
+
+namespace Calculator
+{
+    internal static class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            Calculator.Add(5, 5);
+            Calculator.Add(5, 5);
+            Calculator.Add(5, 5);
+        }
+    }
+
+    internal static class Calculator
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public static double Add(double a, double b)
+        {
+            log.Info("Adddddddddddd111111111!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!d");
+            return a + b;
+        }
+        public static double Subtract(double a, double b)
+        {
+            log.Info("Subtract");
+            return a - b;
+        }
+        public static double Multiply(double a, double b)
+        {
+            log.Info("Multiply");
+            return a * b;
+        }
+        public static double Divide(double a, double b)
+        {
+            log.Info("Divide");
+            return a / b;
+        }
+        public static double Power(double a, double b)
+        {
+            log.Info("Power");
+            return Math.Pow(a, b);
+        }
+    }
+}
---
+++ Calculator/Calculator/Calculator/Test.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+
+
+namespace Calculator
+{
+    public class Test
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        [Theory]
+        [InlineData(6, 3, 9)]
+        [InlineData(4, 3, 7)]
+        [InlineData(2, 3, 5)]
+        public void AddTest(double a, double b, double expect)
+        {
+            log.Info("Add");
+            var result = Calculator.Add(a, b);
+            Assert.Equal(expect, result);
+        }
+
+        [Theory]
+        [InlineData(6, 3, 3)]
+        [InlineData(4, 3, 1)]
+        [InlineData(2, 3, -1)]
+        public void SubtractTest(double a, double b, double expect)
+        {
+            log.Info("SubtractTest");
+            var result = Calculator.Subtract(a, b);
+            Assert.Equal(expect, result);
+        }
+
+        [Theory]
+        [InlineData(6, 3, 18)]
+        [InlineData(4, 3, 12)]
+        [InlineData(2, 3, 6)]
+        public void MultiplyTest(double a, double b, double expect)
+        {
+            log.Info("MultiplyTest");
+            var result = Calculator.Multiply(a, b);
+            Assert.Equal(expect, result);
+        }
+
+        [Theory]
+        [InlineData(6, 3, 2)]
+        [InlineData(11, 2, 5.5)]
+        [InlineData(1, 1, 1)]
+        public void DivideTest(double a, double b, double expect)
+        {
+            log.Info("DivideTest");
+            var result = Calculator.Divide(a, b);
+            Assert.Equal(expect, result);
+        }
+
+        [Theory]
+        [InlineData(6, 3, 216)]
+        [InlineData(11, 2, 121)]
+        [InlineData(498764, 0, 1)]
+        public void PowerTest(double a, double b, double expect)
+        {
+            log.Info("PowerTest");
+            var result = Calculator.Power(a, b);
+            Assert.Equal(expect, result);
+        }
+    }
+}
